e8690e4bfe4b32c86dfeb1d0d2be169ea21dfb84
Yernar17 <ernarmynzhasar8@gmail.com>
2024-11-25 18:56:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// 1 - tapsyrma
+
+// let num = +prompt("1 sandy engiz")
+// let sign = prompt("Tanba engiz")
+// let num2 = +prompt("2 sandy engiz")
+
+// alert(calculator(num, num2, sign));
+
+// function calculator(a, b, c) {
+//     if (num != Number || num2 != Number) {
+//         alert("Qate zhazdyn basynan zhaz");
+//         return;
+//     }
+//     switch(c) {
+//         case "+":
+//             return a + b;
+//         case "-":
+//             return a - b;
+//         case "/":
+//             return a / b;
+//         case "*":
+//             return a * b;
+//         default:
+//             break;
+//     }
+// }
+// console.log(calculator(a,b,c))
+
+
+
+// 2 - tapsyrma
+
+
+// function bagasy() {
+//     let quny = +prompt("Bul zattyn bagasyn engiz: ")
+//     let zat = +prompt("Osydan qansha zat alasyn? ")
+//     let baga = quny * zat
+
+//     if (baga >= 5000){
+//         baga -= baga / 10
+//         alert("Sen " + baga + "tg toleisyn")
+//     }
+
+//     if (baga >= 10000) {
+//         baga -= baga / 20
+//         alert("Sen " + baga + "tg toleisyn")
+//     }
+
+//     if (baga >= 20000) {
+//         baga -= baga / 30
+//         alert("Sen " + baga + "tg toleisyn")
+//     }
+// }
+// alert(bagasy())
+
+
+// 3 - tapsyrma
+
+// let san = +prompt("1 men 100 dyn arasyndagy jasyryn sandy tap")
+
+// let jasyryn = 55;
+
+// function jasyryngr(a) {
+//     if(san == 55) {
+//         alert("Dal taptyn")
+//     } 
+
+//     if (san >= 60){
+//         alert("Сәл төмен")
+//     }
+
+//     if (san <= 50){
+//         alert("Сәл жоғары")
+//     }
+
+//     if (san < 50 ) {
+//         alert("Жоғары")
+//     }
+
+//     if (san > 61 && san >= 61) {
+//         alert("Tөмен")
+//     }
+// }
+// alert(jasyryngr(san))
+
+
+
+// Bonus tapsyrma
+
+let a = 15;
+let b = 20;
+
+function san(a, b){
+a = b
+b = a
+
+}
+console.log(a); 
+console.log(b);
